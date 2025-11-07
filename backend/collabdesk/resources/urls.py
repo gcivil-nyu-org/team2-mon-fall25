e66606c949909994f9c789e7bd260a6c8a80c082
Xingyu Li <xl5936@nyu.edu>
2025-11-07 18:16:51 -0500

    Use S3 to store uploaded files.

--- backend/collabdesk/resources/urls.py
+++ backend/collabdesk/resources/urls.py
@@ -5,4 +5,5 @@ app_name = "resources"
 urlpatterns = [
     path("", ResourceCreateView.as_view(), name="resource-list"),
     path("<uuid:pk>/", ResourceDetailView.as_view(), name="resource-detail"),
+    path("<uuid:pk>/download/", ResourcePresignedUrlView.as_view(), name="resource-download"),
 ]
